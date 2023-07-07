3ef623a8bd4e3d939100568e0f1fae2b8315cba7
Akash <mishrakash1701@gmail.com>
2023-07-08 03:17:41 +0530

    added Nav file

---
+++ lost-and-found-webapp/src/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Nav = () => (
+    <header>
+        <nav>
+            <ul  className='list'>
+                <li ><a className='link head' href="">Lost & Found</a></li>
+                <li><a className='link' href="">Home</a></li>
+                <li><a className='link' href="">Lost Items</a></li>
+                <li><a className='link' href="">Found Item</a></li>
+                <li><a className='link' href="">Report a Lost Item</a></li>
+                <li><a className='link' href="">Report a Found Item</a></li>
+                <li><a className='link' href="">FAQ</a></li>
+                <li><a className='link' href="">Sign In</a></li>
+                <li><a className='link' href="">Sign Up</a></li>
+            </ul>
+        </nav>
+    </header>
+)
+
+export default Nav;
