5ef3760b94d73e8ef58ccd36db8405e8d3fd0a53
Shreyas <shreyas.kamath27@gmail.com>
2023-07-11 16:58:31 +0530

    Dark and light theme switch added

--- lost-and-found-webapp/src/Navbar/Navbar.js
+++ lost-and-found-webapp/src/Navbar/Navbar.js
@@ -3,16 +3,64 @@ import { NavLink } from 'react-router-dom';
 import { FiChevronDown } from 'react-icons/fi';
 import logo from '../images/Logofornavbar.png';
 import './Navbar.css';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Switch from 'react-switch';
+
+const lightTheme = {
+  body: '#f5f5f5',
+  text: '#333',
+  navbarBackground: '#f5f5f5',
+  navbarText: '#333',
+};
+
+const darkTheme = {
+  body: '#333',
+  text: '#f5f5f5',
+  navbarBackground: '#333',
+  navbarText: '#f5f5f5',
+};
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        background-color: ${({ theme }) => theme.body} !important;
+        color: ${({ theme }) => theme.text};
+    }
+
+    .header__middle {
+        background-color: ${({ theme }) => theme.navbarBackground};
+        color: ${({ theme }) => theme.navbarText};
+    }
+
+    .menu-item a {
+        color: ${({ theme }) => theme.navbarText};
+    }
+
+    .sub__menus {
+        background-color: ${({ theme }) => theme.navbarBackground};
+        color: ${({ theme }) => theme.navbarText};
+    }
+
+    .dropdown-menu {
+        background-color: ${({ theme }) => theme.navbarBackground};
+        color: ${({ theme }) => theme.navbarText};
+    }
+`;
 
 const Navbar = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [theme, setTheme] = useState(lightTheme);
 
   const toggleDropdown = () => {
     setDropdownOpen(!isDropdownOpen);
   };
 
-  return (
+  const toggleTheme = () => {
+    setTheme(theme === lightTheme ? darkTheme : lightTheme);
+  };
 
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <header className="header__middle">
         <div className="container">
           <div className="row">
@@ -21,6 +69,18 @@ const Navbar = () => {
               <NavLink exact to="/">
                 <img src={logo} alt="logo" />
               </NavLink>
+              <Switch
+                checked={theme === darkTheme}
+                onChange={toggleTheme}
+                onColor="#04b7f3"
+                onHandleColor="#f5f5f5"
+                handleDiameter={20}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                height={10}
+                width={40}
+                className="theme-toggle-switch"
+              />
             </div>
 
             {/* Nav Items */}
@@ -88,7 +148,7 @@ const Navbar = () => {
           </div>
         </div>
       </header>
-
+    </ThemeProvider>
   );
 };
 
