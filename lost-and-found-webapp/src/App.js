c6fa40021385628dfd7899bcf89a6f41e197b572
lonewolf <thechampthapa@gmail.com>
2023-07-11 15:51:30 +0530

    back to top button integrated

--- lost-and-found-webapp/src/App.js
+++ lost-and-found-webapp/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Navbar from "./Navbar/Navbar";
 import HelpUs from "./help-us-find-page/HelpUs"
 import Home from "./home-page/Home"
+import GoToTop from "./go-to-top/GoToTop"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => (
@@ -15,7 +16,9 @@ const App = () => (
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/helpusfind" element={<HelpUs />} />
             </Routes>
+            <GoToTop/>
         </Router>
+
     </div>
 
 );
