21dfabea7add47c0c17c4e7790a729dfe219815b
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-07-15 15:51:24 +0300

    rectified the banner message

--- lost-and-found-webapp/src/login-page/Login.js
+++ lost-and-found-webapp/src/login-page/Login.js
@@ -9,8 +9,6 @@ export default function Login() {
     <div className='row'>
         <div className='banner-ad col col-sm-6'>
           <h2 className='title'><span style={{color:'#9C27B0'}}>Lost</span>  & <span>Found</span></h2>
-           <h1 className='title'>Welcome !</h1>
-           <p className="ps-5 mt-3" style={{ fontSize: "1rem",fontFamily: "'Poppins', sans-serif" }}>We help you find lost items and reunite them with their owners. Whether you've lost something valuable or found an item that belongs to someone else, we've got you covered. For more info you can checkout </p>
           <div className='login-btn'>
                 <Button variant="contained" color="secondary" style={{ textTransform: "none", borderRadius: "10px", fontFamily: "'Poppins', sans-serif", fontSize: "1.1rem" }}>Login with Outlook</Button>
           </div>
