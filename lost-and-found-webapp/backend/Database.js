b00de96f3358e514e3481278f3fe93f62bbfc7c9
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-07-18 14:11:02 +0300

    updated the helpUsFind page to fetch lost items from the database

--- lost-and-found-webapp/backend/Database.js
+++ lost-and-found-webapp/backend/Database.js
@@ -7,40 +7,40 @@ const uri = 'mongodb+srv://admin:helloadmin@lost-and-found.mczrz51.mongodb.net/L
 const dbName = 'LostandFound';
 
 // Claim the found item and move it to the 'collectedItem' collection
-// async function claimFoundItem(itemId) {
-//   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function claimFoundItem(itemId) {
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-//   try {
-//     // Connect to the MongoDB server
-//     await client.connect();
+  try {
+    // Connect to the MongoDB server
+    await client.connect();
 
-//     // Get a reference to the database
-//     const db = client.db(dbName);
+    // Get a reference to the database
+    const db = client.db(dbName);
 
-//     // Find the item in the 'foundItem' collection
-//     const foundItem = await db.collection('foundItem').findOne({ _id: new ObjectId(2) });
+    // Find the item in the 'foundItem' collection
+    const foundItem = await db.collection('foundItem').findOne({ _id: new ObjectId(2) });
 
-//     if (foundItem) {
-//       // Insert the found item into the 'collectedItem' collection
-//       await db.collection('collectedItem').insertOne(foundItem);
+    if (foundItem) {
+      // Insert the found item into the 'collectedItem' collection
+      await db.collection('collectedItem').insertOne(foundItem);
 
-//       // Remove the item from the 'foundItem' collection
-//       await db.collection('foundItem').deleteOne({ _id: new ObjectId(2) });
+      // Remove the item from the 'foundItem' collection
+      await db.collection('foundItem').deleteOne({ _id: new ObjectId(2) });
 
-//       console.log('Item claimed successfully.');
-//     } else {
-//       console.log('Item not found.');
-//     }
-//   } catch (error) {
-//     console.error('Error occurred:', error);
-//   } finally {
-//     // Close the connection
-//     client.close();
-//   }
-// }
+      console.log('Item claimed successfully.');
+    } else {
+      console.log('Item not found.');
+    }
+  } catch (error) {
+    console.error('Error occurred:', error);
+  } finally {
+    // Close the connection
+    client.close();
+  }
+}
 
-// //Enter Found Item ID here
-// claimFoundItem('');
+//Enter Found Item ID here
+claimFoundItem('');
 
 
 
