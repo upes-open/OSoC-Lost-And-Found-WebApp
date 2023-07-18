85dc70b1ae6cca4afb8207dead6e0709514d2ba0
lonewolf <thechampthapa@gmail.com>
2023-07-18 22:40:13 +0530

    report item routing

--- lost-and-found-webapp/src/help-us-find-page/HelpUs.js
+++ lost-and-found-webapp/src/help-us-find-page/HelpUs.js
@@ -3,6 +3,7 @@ import { Button, Card, CardContent, CardMedia, Typography } from '@mui/material'
 import "./helpUs.css"
 import no_image from "./no-image.png";
 import loading from "./loading.gif";
+import { Link } from "react-router-dom";
 
 const HelpUs = () => {
 
@@ -48,41 +49,48 @@ const HelpUs = () => {
   return (
 
     <>
-      {spinner && (
-        <div className='container-spinner' >
-          <img src={loading} alt="loading" width="40px" />
-        </div>
-      )}
 
-      <Typography variant="h4" className="gradient-text" style={{ textTransform: "none", }} align="center" >
-        <span style={{ fontWeight: '600' }}>Help Us</span> Find
-      </Typography>
 
-      {fetched &&
-        (
-          <div className="cards-container">
-            {lostItems.map((item) => (
-              <Card key={item.id} className="card" style={{ backgroundColor: "whitesmoke" }} >
-                <CardMedia
-                  component="img"
-                  height="150"
-                  image={item.itemImage ? `${host}/lostItemImages/${item.itemImage}` : no_image}
-                  alt={item.name}
-                />
+      <div style={{ height: "100vh" }}>
+        <Typography variant="h4" className="gradient-text" style={{ textTransform: "none", }} align="center" >
+          <span style={{ fontWeight: '600' }}>Help Us</span> Find
+        </Typography>
 
-                <CardContent >
-                  <Typography variant="h6" >
-                    {item.subcategory}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {item.description}
-                  </Typography>
-                  <Button size="small" style={{ fontFamily: "'Poppins', sans-serif", marginTop: "10px", borderRadius: "20px" }} variant="contained" color="secondary" href={`/report/${item.id}`}> REPORT AS FOUND </Button>
-                </CardContent>
-              </Card>
-            ))}
+        {spinner ? (
+          <div className='text-center my-5 pt-5' >
+            <img src={loading} alt="loading" width="40px" />
           </div>
-        )}
+        ) :
+          (fetched  ?
+            (
+              <div className="cards-container">
+                {lostItems.map((item) => (
+                  <Card key={item.id} className="card" style={{ backgroundColor: "whitesmoke" }} >
+                    <CardMedia
+                      component="img"
+                      height="150"
+                      image={item.itemImage ? `${host}/lostItemImages/${item.itemImage}` : no_image}
+                      alt={item.name}
+                    />
+
+                    <CardContent >
+                      <Typography variant="h6" >
+                        {item.subcategory}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {item.description}
+                      </Typography>
+                      <Button size="small" style={{ fontFamily: "'Poppins', sans-serif", marginTop: "10px", borderRadius: "20px" }} variant="contained" color="secondary" component={Link} to="/found"> REPORT AS FOUND </Button>
+                    </CardContent>
+                  </Card>
+                ))}
+              </div>
+            ) : (
+              <div className="text-center my-5 pt-5">
+                <h4>No items to display ...</h4>
+              </div>
+            ))}
+      </div>
     </>
   );
 };
