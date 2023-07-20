7311d15188662ee1c35f5078aace9d98baa11578
Akash <mishrakash1701@gmail.com>
2023-07-20 23:16:00 +0530

    added footer

--- lost-and-found-webapp/src/Footer/Footer.js
+++ lost-and-found-webapp/src/Footer/Footer.js
@@ -1,83 +1,78 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./Footer.css";
-import image from "../images/insta-logo.png";
-import image1 from "../images/twitter-logo.png";
-import image2 from "../images/facebook-logo.png";
-import { NavLink } from "react-router-dom";
-
-
+import fb from "../images/insta-logo.png";
+import twitter from "../images/twitter-logo.png";
+import insta from "../images/facebook-logo.png";
 const Footer = () => {
-  const [isDropdownOpen, setDropdownOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!isDropdownOpen);
-  };
-
   return (
     <div className="footer">
-      <div className="content">
-      {/* <NavLink to="/about" activeClassName="active-link"><p>About Us</p></NavLink> */}
-      <NavLink to="/about" activeClassName="active-link" className="nav-link">
-  <p>About Us </p>
-</NavLink>
-
-        <p>Services</p>
-        <NavLink to="/feedback" activeClassName="active-link" className="nav-link"><p>Feedback </p></NavLink>
-        <p
-          className="menu-item"
-          onMouseEnter={toggleDropdown}
-          onMouseLeave={toggleDropdown}
-        >
-          Social
-          {isDropdownOpen && (
-            <div className="dropdown">
-              {/* Dropdown content */}
-              <ul className="dropdownmenu">
-                <li>
-                  <a href='https://www.instagram.com/upes_dehradun/'><img src={image} className="instapic" alt='insta-pic'/><span className="text">Instagram</span></a>
-                </li>
-                <li>
-                  <a href='https://twitter.com/UPESDehradun' ><img src={image1} className="instapic" alt='twitter-pic' /><span className="text">Twitter</span></a>
-                </li>
-                <li>
-                  <a href='https://www.facebook.com/UPESddnuk/' > <img src={image2} className="instapic" alt='facebook-pic' /><span className="text">Facebook</span></a>
-                </li>
-              </ul>
+      <div className="sb__footer__section__padding">
+        <div className="sb__footer-links">
+          <div className="sb__footer-links_div">
+            <h4>About Us</h4>
+            <a href="/home">
+              <p>Home</p>
+            </a>
+            <a href="/feedback">
+              <p>Feedback</p>
+            </a>
+          </div>
+          <div className="sb__footer-links_div">
+            <h4>Services</h4>
+            <a href="/lost">
+              <p>Report Lost Item</p>
+            </a>
+            <a href="/found">
+              <p>Report Found Item</p>
+            </a>
+            <a href="/items">
+              <p>See Items Gallery</p>
+            </a>
+            <a href="/helpusfind">
+              <p>Help Us Find</p>
+            </a>
+          </div>
+          <div className="sb__footer-links_div">
+            <h4>Partners</h4>
+            <a href="/about">
+              <p>UPES Open Community</p>
+            </a>
+          </div>
+          <div className="sb__footer-links_div">
+            <h4>Feedback</h4>
+            <a href="/resources">
+              <p>Email</p>
+            </a>
+            <a href="/resources">
+              <p>Contact Number</p>
+            </a>
+            <a href="/resources">
+              <p>Visit Us</p>
+            </a>
+          </div>
+          <div className="sb__footer-links_div">
+            <h4>Social</h4>
+            <div className='socialmedia'>
+            <a href="https://www.instagram.com/upes_dehradun/"><p><img src={fb} alt="instaimage"/></p></a>
+            <a href="https://twitter.com/UPESDehradun/"><p><img src={twitter} alt="twitterimage"/></p></a>
+            <a href="https://www.facebook.com/UPESddnuk/"><p><img src={insta} alt="fbimage"/></p></a>
             </div>
-          )}
-        </p>
-      </div>
-
-      <div className="main-div">
-        <div className="contentname1">
-          <NavLink to="/home" className = "navlinkhome removeUnderline"><p>Home </p></NavLink>
-          <NavLink to="/feedback" className = "navlinkhome removeUnderline"><p>Feedbacks</p></NavLink>
-          
+          </div>
         </div>
+        <hr></hr>
 
-        <div className="contentname2">
-        <NavLink to="/lost" className = "navlinkhome removeUnderline"><p>Report Lost Item</p></NavLink>
-        <NavLink to="/found" className = "navlinkhome removeUnderline"><p>Report Found Item</p></NavLink>
-        <NavLink to="/items" className = "navlinkhome removeUnderline"><p>See Items Gallery</p></NavLink>
-        <NavLink to="/helpusfind" className = "navlinkhome removeUnderline"><p>Help Us Find</p></NavLink>
-        </div>
-
-        <div className="contentname3">
-          <p>Email</p>
-          <p>Contact Number</p>
-          <p>Visit Us</p>
-        </div>
-
-        <div className="contentname4">
-          <p>
-            <img src={image} className="instapic" alt='insta-pic'  /><a className="removeUnderline"  href='https://www.instagram.com/upes_dehradun/'><span className="text ">Instagram</span></a>
-          </p>
-          <p>
-            <img src={image1} className="instapic" alt='twitter-pic' /><a className="removeUnderline" href='https://twitter.com/UPESDehradun'><span className="text">Twitter</span></a>
-          </p>
-          <p>
-          <img src={image2} className="instapic" alt='facebook-pic' /><a className="removeUnderline" href='https://www.facebook.com/UPESddnuk/' ><span className="text">Facebook</span></a>
-          </p>
+        <div className='sb__footer-below'>
+          <div className='sb__footer-copyright'>
+            <p>
+              @{new Date().getFullYear()} CodeInn. All right reserved.
+            </p>
+          </div>
+          <div className='sb__footer-below-links'>
+            <a href="/terms"><div><p>Terms & Conditions</p></div></a>
+            <a href="/terms"><div><p>Privacy</p></div></a>
+            <a href="/terms"><div><p>Security</p></div></a>
+            <a href="/terms"><div><p>Cookie Declaration</p></div></a>
+          </div>
         </div>
       </div>
     </div>
