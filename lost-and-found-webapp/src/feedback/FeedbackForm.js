20f8dcea0bc3ef28c954cf9e2587e4fcbb59359f
lonewolf <thechampthapa@gmail.com>
2023-07-21 15:02:54 +0530

    made changes to feedback page

--- lost-and-found-webapp/src/feedback/FeedbackForm.js
+++ lost-and-found-webapp/src/feedback/FeedbackForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './feedback.css';
 import { Button, TextField } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
 const FeedbackForm = (props) => {
   const [details, setDetails] = React.useState({ email: "", feedback: "" });
@@ -10,6 +11,20 @@ const FeedbackForm = (props) => {
     setDetails({ ...details, [e.target.name]: e.target.value })
   }
 
+  const CssTextField = styled(TextField)({
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: `${props.theme === 'dark' ? 'grey' : '#333'}`,
+      },
+      '&:hover fieldset': {
+        borderColor: `${props.theme === 'dark' ? 'grey' : '#333'}`,
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: 'purple',
+      },
+      
+    },
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -26,71 +41,72 @@ const FeedbackForm = (props) => {
 
     const json = await response.json();
     console.log(json);
-    setDetails({email:"", feedback:""});
+    setDetails({ email: "", feedback: "" });
   }
 
   return (
     <>
-        <div className={`body ${props.theme === 'dark' ? 'dark-mode' : ''}`}></div>
-          <div className="addnotes "  style={{ paddingTop: "50px" ,margin:"auto" }}>
-            <div className=" right ps-5 pe-5 pt-4 mb-5" style={{backgroundColor: `${props.theme === 'dark' ? '#333' : ''}`, color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}`}}>
-              <div className="d-flex justify-content-center mb-2">
-                <h3 style={{ fontWeight: "bold", fontFamily: '"Mons", sans-serif' }}>Feedback Form</h3>
-              </div>
-              <p className="mb-4 text-center">Thank you for taking your time to provide feedback. We appreciate hearing from you and will review your comments carefully.</p>
-
-              <form className={`feedback-form ${props.theme === 'dark' ? 'dark-mode' : ''}`} onSubmit={handleSubmit}>
-                <div className="mb-4">
-                  <p className='container' style={{ marginBottom: "6px" }}>Email</p>
-                  <TextField variant="outlined" color="secondary" type="email" onChange={onChange} value={details.email} placeholder='Email' id="email" name="email"
-                    InputProps={{
-                      style: {
-                        borderRadius: '10px',
-                        color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}`,
-                      },
-                    }} />
-                </div>
-
-                <h6 className="container mt-4" style={{ marginBottom: "-1px" }}>How would you rate us?</h6>
-                <div className='container-star '>
-                  <div className="star-widget">
-                    <input type="radio" name="rate" id="rate-5" />
-                    <label htmlFor="rate-5" className='fas fa-star' ></label>
-
-                    <input type="radio" name="rate" id="rate-4" />
-                    <label htmlFor="rate-4" className='fas fa-star'></label>
-
-                    <input type="radio" name="rate" id="rate-3" />
-                    <label htmlFor="rate-3" className='fas fa-star'></label>
-
-                    <input type="radio" name="rate" id="rate-2" />
-                    <label htmlFor="rate-2" className='fas fa-star'></label>
-
-                    <input type="radio" name="rate" id="rate-1" />
-                    <label htmlFor="rate-1" className='fas fa-star'></label>
-                    <header></header>
-                  </div>
-                </div>
-
-                <div className="my-3">
-                  <p className="container" style={{ marginBottom: "6px" }}>Please share your feedback</p>
-                  <TextField type="text" placeholder='Share you experience or suggestions' multiline rows={5} onChange={onChange} value={details.feedback} id="feedback" name="feedback" color="secondary" variant="outlined" required
-
-                    InputProps={{
-                      style: {
-                        borderRadius: '20px',
-                        color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}`,
-                      },
-
-                    }} />
-
-                </div>
-
-                <Button type="submit" variant="contained" color="success" className="my-3 mb-4"style={{ textTransform: "none", borderRadius: "15px", fontFamily: "'Poppins', sans-serif", fontSize: "1rem" }}>Submit</Button>
-              </form>
-            </div>
+      <div className={`body ${props.theme === 'dark' ? 'dark-mode' : ''}`}></div>
+      <div className="addnotes " style={{ paddingTop: "50px", margin: "auto" }}>
+        <div className=" right ps-5 pe-5 pt-4 mb-5" style={{ backgroundColor: `${props.theme === 'dark' ? '#333' : ''}`, color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}` }}>
+          <div className="d-flex justify-content-center mb-2">
+            <h3 style={{ fontWeight: "bold", fontFamily: '"Mons", sans-serif' }}>Feedback Form</h3>
           </div>
-  
+          <p className="mb-4 text-center">Thank you for taking your time to provide feedback. We appreciate hearing from you and will review your comments carefully.</p>
+
+          <form className={`feedback-form ${props.theme === 'dark' ? 'dark-mode' : ''}`} onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <p className='container' style={{ marginBottom: "6px" }}>Email</p>
+              <CssTextField variant="outlined" color="secondary" type="email" onChange={onChange} value={details.email} placeholder='Email' id="email" name="email"
+                InputProps={{
+                  style: {
+                    borderRadius: '10px',
+                    color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}`,
+                  },
+                }} />
+            </div>
+
+            <h6 className="container mt-4" style={{ marginBottom: "-1px" }}>How would you rate us?</h6>
+            <div className='container-star '>
+              <div className="star-widget">
+                <input type="radio" name="rate" id="rate-5" />
+                <label htmlFor="rate-5" className='fas fa-star' ></label>
+
+                <input type="radio" name="rate" id="rate-4" />
+                <label htmlFor="rate-4" className='fas fa-star'></label>
+
+                <input type="radio" name="rate" id="rate-3" />
+                <label htmlFor="rate-3" className='fas fa-star'></label>
+
+                <input type="radio" name="rate" id="rate-2" />
+                <label htmlFor="rate-2" className='fas fa-star'></label>
+
+                <input type="radio" name="rate" id="rate-1" />
+                <label htmlFor="rate-1" className='fas fa-star'></label>
+                <header></header>
+              </div>
+            </div>
+
+            <div className="my-3">
+              <p className="container" style={{ marginBottom: "6px" }}>Please share your feedback</p>
+              <CssTextField type="text" placeholder='Share you experience or suggestions' multiline rows={5} onChange={onChange} value={details.feedback} id="feedback" name="feedback" color="secondary" variant="outlined" required
+
+                InputProps={{
+                  style: {
+                    borderRadius: '20px',
+                    color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}`,
+                    className: "change",
+                  },
+
+                }} />
+
+            </div>
+
+            <Button type="submit" variant="contained" color="success" className="my-3 mb-4" style={{ textTransform: "none", borderRadius: "15px", fontFamily: "'Poppins', sans-serif", fontSize: "1rem" }}>Submit</Button>
+          </form>
+        </div>
+      </div>
+
     </>
   )
 }
