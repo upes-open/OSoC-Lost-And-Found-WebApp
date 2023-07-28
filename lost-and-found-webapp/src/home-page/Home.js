1f4e2d76a6fca9bf5a6aae87dd36d49265fc4f67
Akash <mishrakash1701@gmail.com>
2023-07-28 13:42:21 +0530

    made the buttons same size

--- lost-and-found-webapp/src/home-page/Home.js
+++ lost-and-found-webapp/src/home-page/Home.js
@@ -4,6 +4,7 @@ import image from './home1.png';
 import { Button } from '@mui/material';
 import { useMsal } from "@azure/msal-react";
 import { NavLink } from 'react-router-dom';
+
 const HomePage = (props) => {
   const { instance } = useMsal();
 
@@ -32,25 +33,46 @@ const HomePage = (props) => {
   return (
     <>
       <div style={{ height: "100vh" }}>
-        <div className="container-fluid" >
-          <div className="row" style={{color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}`}}>
+        <div className="container-fluid">
+          <div className="row" style={{ color: `${props.theme === 'dark' ? '#f5f5f5' : '#333'}` }}>
             <div className="col-md-5 ps-5">
               <h1 className="display-3 pt-5 mt-5 ps-5"><span style={{ color: "#9C27B0" }}>hi, </span>Welcome!</h1>
               <p className="ps-5 respo changing-text" style={{ fontSize: "1.7rem", fontWeight: "bold", marginLeft: "80px" }}>{text} </p>
               <p className="ps-5 mt-3" style={{ fontSize: "1rem" }}>We help you find lost items and reunite them with their owners. Whether you've lost something valuable or found an item that belongs to someone else, we've got you covered. For more info you can checkout </p>
               <div className="d-flex justify-content-center">
-              <NavLink to="/about" style={{ textDecoration: 'none' }}>
-              <Button variant="contained" color="secondary" style={{
-      textTransform: "none",
-      borderRadius: "20px",
-      fontFamily: "'Poppins', sans-serif",
-      fontSize: "1.1rem"
-    }}
-  >
-    About Us
-  </Button>
-</NavLink>
-                <Button  href="#" onClick={handleSignOut} variant="contained" color="secondary" style={{ marginLeft: "20px", textTransform: "none", borderRadius: "20px", fontFamily: "'Poppins', sans-serif", fontSize: "1.1rem" }}>Sign Out</Button>
+                {/* Use the same className for both buttons */}
+                <NavLink to="/about" style={{ textDecoration: 'none' }}>
+                  <Button
+                    className="sign-out-button" // Use the same class name as the "Sign Out" button
+                    variant="contained"
+                    color="secondary"
+                    style={{
+                      textTransform: "none",
+                      borderRadius: "20px",
+                      fontFamily: "'Poppins', sans-serif",
+                      fontSize: "1.1rem"
+                    }}
+                  >
+                    About Us
+                  </Button>
+                </NavLink>
+                <NavLink to="" style={{ textDecoration: 'none' }}>
+                  <Button
+                    className="sign-out-button" // Use the same class name as the "Sign Out" button
+                    onClick={handleSignOut}
+                    variant="contained"
+                    color="secondary"
+                    style={{
+                      marginLeft: "20px",
+                      textTransform: "none",
+                      borderRadius: "20px",
+                      fontFamily: "'Poppins', sans-serif",
+                      fontSize: "1.1rem"
+                    }}
+                  >
+                    Sign Out
+                  </Button>
+                </NavLink>
               </div>
             </div>
             <div className="col-md-7 pt-5 d-flex flex-column align-items-center">
